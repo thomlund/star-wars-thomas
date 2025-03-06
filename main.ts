594abe26e7440382423508d2aace8c31245db434
thomlund <t.lundgaard@outlook.dk>
2025-03-06 20:11:13 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-scene.setBackgroundImage(assets.image`thomas`)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+scene.setBackgroundImage(assets.image`baggrund1`)
+scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`rumskib1`, SpriteKind.Player))
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
 info.setLife(3)
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
