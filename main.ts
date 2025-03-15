da9f79a58f92b8775d1d6d313e7f159765b88e1c
thomlund <t.lundgaard@outlook.dk>
2025-03-15 11:31:47 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    for (let value of sprites.allOfKind(SpriteKind.Player)) {
+        projectile = sprites.createProjectileFromSprite(assets.image`skud`, value, 50, 0)
+    }
 })
+let projectile: Sprite = null
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`rumskib1`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`rumskib2`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`rumskib3`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`rumskib4`, SpriteKind.Player))
 scene.setBackgroundImage(assets.image`baggrund1`)
-scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
-mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-info.setLife(3)
+for (let value of mp.allPlayers()) {
+    mp.moveWithButtons(value)
+}
+for (let value of sprites.allOfKind(SpriteKind.Player)) {
+    value.setStayInScreen(true)
+    info.setLife(3)
+}
